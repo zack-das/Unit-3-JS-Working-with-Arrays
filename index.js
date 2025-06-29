f523e5ea0b2947f65b0955e8058d5cbf004a3d04
zack-das <izackdas@gmail.com>
2025-06-29 17:55:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution here!
+// Write your solution here!y
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
